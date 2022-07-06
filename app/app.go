c96517d4ff2f90baa02a4770dd0745c7a5cd223a
ferryanggriawan <ferrya1997@gmail.com>
2022-07-06 16:22:36 +0700

    error handler http

--- app/app.go
+++ app/app.go
@@ -1,3 +1,22 @@
 package app
 
-import _ "todo_app_api_go/app/database"
+import (
+	"fmt"
+	_ "todo_app_api_go/app/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Print(abc interface{}) {
+	fmt.Printf("%s", abc)
+}
+
+func ErrorHttp(ctx *gin.Context) {
+	if ctx.Errors != nil {
+		resp := gin.H{
+			"message": "unknown error",
+		}
+
+		ctx.JSON(500, resp)
+	}
+}
