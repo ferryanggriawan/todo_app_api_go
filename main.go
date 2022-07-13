fb06055371e79a5fa78de028587f7701f51d43e3
ferryanggriawan <ferrya1997@gmail.com>
2022-07-13 16:45:56 +0700

    recover when panic

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	_ "todo_app_api_go/app"
-	"todo_app_api_go/app/database"
+	_ "todo_app_api_go/app/database"
 	"todo_app_api_go/route"
 )
 
 func main() {
 	const port string = ":9000"
 
-	db := &database.Connection{}
-	db.Initialize()
-
 	appRoute := &route.Route{}
 	appRoute.Initialize()
-	appRoute.Setup()
 
-	appRoute.Router.Run(port)
+	defer recover()
+	err := appRoute.Router.Run(port)
+	if err != nil {
+		panic("App Terminated")
+	}
 }
