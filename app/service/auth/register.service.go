abc576ec4d1150bc9a07d415de4699ed65c0f238
ferryanggriawan <ferrya1997@gmail.com>
2022-07-13 16:45:46 +0700

    learn panic defer recover

--- app/service/auth/register.service.go
+++ app/service/auth/register.service.go
@@ -1,6 +1,38 @@
-package auth
+package auth_service
 
-func RegisterUser() string {
-	resp := ""
-	return resp
+import (
+	"encoding/json"
+	"todo_app_api_go/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUser(c *gin.Context) *model.User {
+	defer handleError()
+
+	userForm := checkRequest(c)
+	user := insertToDb(userForm)
+
+	return user
+}
+
+func checkRequest(c *gin.Context) *model.UserForm {
+	user := model.UserForm{}
+
+	decoder := json.NewDecoder(c.Request.Body)
+	if err := decoder.Decode(&user); err != nil {
+		panic("Bad Request")
+	}
+
+	return &user
+}
+
+func insertToDb(m *model.UserForm) *model.User {
+	user := &model.User{}
+
+	return user
+}
+
+func handleError() {
+
 }
